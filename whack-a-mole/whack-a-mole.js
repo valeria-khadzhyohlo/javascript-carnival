75f921ad2748217071663c0336231ee3105d44df
Valeriia Volkova <valeriiavolkova@Valeriias-Air.fritz.box>
2022-05-12 19:32:03 +1200

    add solutions to whack-a-mole

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -2,4 +2,32 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
+let cells = document.getElementsByTagName('TD')
+let moleImage = document.createElement('img')
+moleImage.classList.add('mole')
+moleImage.src='mole.PNG'
+
+
+clicked =()=>{
+  moleImage.onclick=whackedMole()
+  moleImage.onclick = ()=>{
+    play()
+    moleImage.remove();
+    whackedMole()
+  }
+  }
+
+//Random mole show up
+whackedMole = () =>{
+  let randomIndex = Math.floor(Math.random()*24)
+  let cell = cells[randomIndex]
+  cell.appendChild(moleImage)
+}
+clicked()
+
+let play =()=> new Audio(src='whack-audio.wav').play()
+
+
+
+
 console.log("Whack-a-Mole!")
